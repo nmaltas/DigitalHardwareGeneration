74285e29f8eb3e6e3e113f983677cb6f657fc773
Maltas <silmatien56@gmail.com>
2026-03-13 16:25:38 -0400

    - Discovered a better way to format strings with the C++20 standard. - ROMW generatoin finished and verified.

--- Generator/Generator.cpp
+++ Generator/Generator.cpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <vector>
 #include <fstream>
+#include <format>
 
 #include "Parameters.hpp"
 
@@ -25,6 +26,8 @@ int main()
     PrintTables(Specs);
 
     GenerateROMB(Specs);
+
+    GenerateROMW(Specs);
 
     cout << "Hey!!" << endl;
     return 0;
@@ -95,22 +98,23 @@ void GenerateROMB(const Parameters &Specs)
 {
     ofstream Output;
     Output.open("ROMB.vhd");
+    string Temp;
 
     Output << Specs.Libraries;
-    Output << R"VHDL(
+    Output << format(R"VHDL(
 entity ROMB is
 
   generic (
-    DataWidth : integer := )VHDL"
-           << Specs.T << R"VHDL(;
+    DataWidth : integer := {};
 
     -- Memory slots have to be hardcodded. HLS will deal with this.
-)VHDL";
+)VHDL",
+                     Specs.T);
 
-    // Dynamic values #
+    // Dynamic value generics.
     for (int i = 0; i < Specs.M; i++)
     {
-        Output << "    Slot" << i << " : integer := " << i;
+        Output << format("    Slot{0} : integer := {0}", i);
 
         if (i == Specs.M - 1)
         {
@@ -131,10 +135,10 @@ entity ROMB is
     -- Number of output ports has to be hardcoded. HLS will have to deal with this.
 )VHDL";
 
-    // Dynamic Port #
+    // Dynamic Ports.
     for (int i = 0; i < Specs.M; i++)
     {
-        Output << "    DataOut" << i << " : out signed ((DataWidth - 1) downto 0)";
+        Output << format("    DataOut{} : out signed ((DataWidth - 1) downto 0)", i);
 
         if (i == Specs.M - 1)
         {
@@ -153,10 +157,10 @@ architecture ROMB1 of ROMB is
   -- Hardcoded by HLS.
 )VHDL";
 
-    // Dynamic memory slot signal #
+    // Dynamic memory slot signal #.
     for (int i = 0; i < Specs.M; i++)
     {
-        Output << "  signal MemSlot" << i << " : signed ((DataWidth - 1) downto 0);" << endl;
+        Output << format("  signal MemSlot{} : signed ((DataWidth - 1) downto 0);", i) << endl;
     }
 
     Output << R"VHDL(
@@ -166,10 +170,10 @@ begin
   -- Hardcoded by HLS.
 )VHDL";
 
-    // Dynamic values to memory slot assignment
+    // Dynamic values to memory slot assignment.
     for (int i = 0; i < Specs.M; i++)
     {
-        Output << "  MemSlot" << i << " <= to_signed(Slot" << i << ", MemSlot" << i << "'length);" << endl;
+        Output << format("  MemSlot{0} <= to_signed(Slot{0}, MemSlot{0}'length);", i) << endl;
     }
 
     Output << R"VHDL(
@@ -181,18 +185,18 @@ begin
       if (Enable = '1') then
 )VHDL";
 
-    // Dynamic # of slots enabled
+    // Dynamic # of slots enabled.
     for (int i = 0; i < Specs.M; i++)
     {
-        Output << "        DataOut" << i << " <= MemSlot" << i << ";" << endl;
+        Output << format("        DataOut{0} <= MemSlot{0};", i) << endl;
     }
 
     Output << "      else" << endl;
 
-    // Dynamic # of slots disabled
+    // Dynamic # of slots disabled.
     for (int i = 0; i < Specs.M; i++)
     {
-        Output << "        DataOut" << i << " <= (others => 'Z');" << endl;
+        Output << format("        DataOut{} <= (others => 'Z');", i) << endl;
     }
 
     Output << R"VHDL(      end if;
@@ -209,21 +213,134 @@ end ROMB1;)VHDL";
 void GenerateROMW(const Parameters &Specs)
 {
     ofstream Output;
-    Output.open("ROMB.vhd");
+    Output.open("ROMW.vhd");
 
     Output << Specs.Libraries;
 
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
-    Output << R"VHDL()VHDL";
+    Output << format(R"VHDL(
+entity ROMW is
+
+  generic (
+    DataWidth : integer := {};
+    Columns   : integer := {};
+
+    -- These memory slots need to be hardcoded. HLS will have to deal with this.
+)VHDL",
+                     Specs.T, Specs.N);
+
+    // Dynamic value generics.
+    for (int i = 0; i < Specs.M; i++)
+    {
+        for (int j = 0; j < Specs.N; j++)
+        {
+            Output << format("    Slot{0}{1} : integer := {2}", i, j, ((i * 4) + j));
+
+            if ((i * 4) + j == ((Specs.M * Specs.N) - 1))
+            {
+                Output << ");" << endl;
+            }
+            else
+            {
+                Output << ";" << endl;
+            }
+        }
+    }
+
+    Output << R"VHDL(
+  port (
+    Address : in integer range 0 to (Columns - 1);
+    Enable  : in std_logic;
+    Clk     : in std_logic;
+
+    -- One output port is needed for each Row in order to support full parallelization.
+    -- Number of output ports has to be hardcoded. HLS will have to deal with this.
+)VHDL";
+
+    // Dynamic ports.
+    for (int i = 0; i < Specs.M; i++)
+    {
+        Output << format("    DataOut{} : out signed ((DataWidth - 1) downto 0)", i);
+        if (i == Specs.M - 1)
+        {
+            Output << "\n  );" << endl;
+        }
+        else
+        {
+            Output << ";" << endl;
+        }
+    }
+
+    Output << R"VHDL(end ROMW;
+
+architecture ROMW1 of ROMW is
+  type MemoryArray is array ((Columns - 1) downto 0) of signed ((DataWidth - 1) downto 0);
+
+  -- Hardcoded by HLS
+)VHDL";
+
+    // Dynamic memory slots declaration.
+    for (int i = 0; i < Specs.M; i++)
+    {
+        Output << format("  signal Row{} : MemoryArray;", i) << endl;
+    }
+
+    Output << R"VHDL(
+begin
+
+  -- Values are assigned to the arrays here.
+
+  -- Hardcoded by HLS.
+)VHDL";
+
+    // Dynamic memory slots value assignment.
+    for (int i = 0; i < Specs.M; i++)
+    {
+        for (int j = 0; j < Specs.N; j++)
+        {
+            Output << format("  Row{0}({1}) <= to_signed(Slot{0}{1}, DataWidth);", i, j) << endl;
+        }
+
+        Output << endl;
+    }
+
+    Output << R"VHDL(  process (Clk) is
+  begin
+    if ((Clk'event) and (Clk = '1')) then
+
+      -- Only output when Enable is asserted.
+      if (Enable = '1') then
+
+        -- Guarding against Invalid Address values.
+        if (Address < Columns) then
+)VHDL";
+
+    // Data out control.
+    for (int i = 0; i < Specs.M; i++)
+    {
+        Output << format("          DataOut{0} <= Row{0}(Address);", i) << endl;
+    }
+
+    Output << "        else" << endl;
+
+    for (int i = 0; i < Specs.M; i++)
+    {
+        Output << format("          DataOut{} <= (others => '1');", i) << endl;
+    }
+
+    Output << R"VHDL(        end if;
+
+      else
+)VHDL";
+
+    for (int i = 0; i < Specs.M; i++)
+    {
+        Output << format("        DataOut{} <= (others => 'Z');", i) << endl;
+    }
+
+    Output << R"VHDL(      end if;
+    end if;
+  end process;
+end ROMW1;)VHDL";
 
     Output.close();
 }
