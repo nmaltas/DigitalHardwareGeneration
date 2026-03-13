45bef375fd95fa487ad8b1a42e4b5c58747b4b25
Maltas <silmatien56@gmail.com>
2026-03-13 16:59:35 -0400

    Finished generating and verifying Memory Module.

--- Generator/Generator.cpp
+++ Generator/Generator.cpp
@@ -14,6 +14,9 @@ void PrintTables(const Parameters &Specs);
 void GenerateROMB(const Parameters &Specs);
 //
 //
+void GenerateMemoryX(const Parameters &Specs);
+//
+//
 void GenerateROMW(const Parameters &Specs);
 //
 //
@@ -26,7 +29,7 @@ int main()
     PrintTables(Specs);
 
     GenerateROMB(Specs);
-
+    GenerateMemoryX(Specs);
     GenerateROMW(Specs);
 
     cout << "Hey!!" << endl;
@@ -204,6 +207,77 @@ begin
   end process;
 
 end ROMB1;)VHDL";
+
+    Output.close();
+}
+//
+//
+// Generate ROMX VHDL file
+void GenerateMemoryX(const Parameters &Specs)
+{
+    ofstream Output;
+    Output.open("MemoryModule.vhd");
+
+    Output << Specs.Libraries;
+
+    Output << format(R"VHDL(library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+use ieee.numeric_std.all;
+
+entity MemoryModule is
+
+  generic (
+    DataWidth : integer := 8;
+    MemSize   : integer := {});
+
+  port (
+    DataIn  : in signed ((DataWidth - 1) downto 0);
+    Address : in integer range 0 to (MemSize - 1);
+    WE      : in std_logic;
+    RE      : in std_logic;
+    Clk     : in std_logic;
+
+    DataOut : out signed (DataWidth - 1 downto 0)
+  );
+end MemoryModule;
+
+architecture MemoryModule1 of MemoryModule is
+  type MemoryArray is array ((MemSize - 1) downto 0) of signed ((DataWidth - 1) downto 0);
+
+  signal Data : MemoryArray;
+
+begin
+
+  process (Clk)
+  begin
+    if (Clk'event) and (Clk = '1') then
+
+      if (WE = '1') then
+        -- Guard against invalid Address values.
+        if (Address   <= MemSize) then
+          Data(Address) <= DataIn;
+          DataOut       <= (others => 'Z');
+        else
+          DataOut <= (others => '1');
+        end if;
+
+      else
+        if (RE = '1') then
+          -- Guard against invalid Address values.
+          if (Address < MemSize) then
+            DataOut <= Data(Address);
+          else
+            DataOut <= (others => '1');
+          end if;
+        else
+          DataOut <= (others => 'Z');
+        end if;
+      end if;
+    end if;
+  end process;
+end MemoryModule1;
+)VHDL",
+                     Specs.N);
 
     Output.close();
 }
